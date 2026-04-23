f028a31cdb81ef733cc17fcc2684c2599cc6fefd
知秋 <127842603+947409161@users.noreply.github.com>
2026-04-23 16:30:49 +0800

    Update and rename rezygisk.sh to .rezygisk.sh
    Signed-off-by: 知秋 <127842603+947409161@users.noreply.github.com>

---
+++ module/src/.rezygisk.sh
@@ -0,0 +1,7 @@
+MODULE_DIR="/data/adb/modules/rezygisk"
+THIS_SCRIPT="/data/adb/service.d/.rezygsik.sh"
+
+if [ ! -d "$MODULE_DIR/disable" ]; then
+  cat "$MODULE_DIR/module.prop.orig" >"$MODULE_DIR/module.prop"
+  rm -f "$THIS_SCRIPT"
+fi
