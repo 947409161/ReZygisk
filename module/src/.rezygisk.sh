b54caa7114e8c5f072226854c2be22f541baf7aa
知秋 <127842603+947409161@users.noreply.github.com>
2026-04-23 16:37:21 +0800

    Update .rezygisk.sh
    Signed-off-by: 知秋 <127842603+947409161@users.noreply.github.com>

--- module/src/.rezygisk.sh
+++ module/src/.rezygisk.sh
@@ -2,6 +2,6 @@ MODULE_DIR="/data/adb/modules/rezygisk"
 THIS_SCRIPT="/data/adb/service.d/.rezygsik.sh"
 
 if [ ! -d "$MODULE_DIR/disable" ]; then
-  cat "$MODULE_DIR/module.prop.orig" >"$MODULE_DIR/module.prop"
+  cat "$MODULE_DIR/module.prop.bak" >"$MODULE_DIR/module.prop"
   rm -f "$THIS_SCRIPT"
 fi
